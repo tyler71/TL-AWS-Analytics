9bc1cd2865fd221013d84111899dc0f7b2c72b8c
Tyler <4926565+tyler71@users.noreply.github.com>
2022-02-08 15:21:01 -0800

    Comment needed variables for main.sh as well

--- main.sh
+++ main.sh
@@ -3,7 +3,14 @@
 export MPLCONFIGDIR=$HOME/cache/streamlit/mpl
 mkdir -p $MPLCONFIGDIR
 
+# For Replit
 unset DISPLAY
+
+# STREAMLIT_SERVER_ADDRESS          = "0.0.0.0"
+# STREAMLIT_SERVER_PORT             = "8501"
+# STREAMLIT_SERVER_COOKIESECRET     = "cookieSecret2"
+# STREAMLIT_BROWSER_SERVERADDRESS   = "https://domain.tld"
+# STREAMLIT_CLIENT_SHOWERRORDETAILS = "false"
 
 echo "Starting server.."
 streamlit run main.py        \
