a989667f9c849dbe35b9239454fa49f34c2729f2
Tyler <4926565+tyler71@users.noreply.github.com>
2022-02-07 18:35:26 -0800

    Allow prefix and suffix for sliding window
    Will be useful for easily generating the strings needed

--- modules/helper.py
+++ modules/helper.py
@@ -4,11 +4,11 @@ import os
 
 from datetime import datetime, timedelta
 
-def get_window_days(days: int) -> typing.Generator[str, None, None]:
+def get_window_days(days: int, prefix='', suffix='') -> typing.Generator[str, None, None]:
     tz = pytz.timezone(os.getenv('TIMEZONE', 'America/Los_Angeles')
     today = datetime.now(tz)
     one_day = timedelta(days=1)
 
     for i in range(days):
         calc_date = today - one_day * i
-        yield calc_date.strftime('%Y/%m/%d')
+        yield ''.join((prefix, calc_date.strftime('%Y/%m/%d'), suffix))
