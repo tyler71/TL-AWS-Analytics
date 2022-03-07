7cfceb28d0ee2fd48f9dd06d61c9a133c02720f6
Tyler <4926565+tyler71@users.noreply.github.com>
2022-03-07 08:36:48 -0800

    refactor queue_wait_list

--- app/reports/ctr_debug/queue_wait_list.py
+++ app/reports/ctr_debug/queue_wait_list.py
@@ -12,12 +12,13 @@ from modules.helper import show_empty_dec
 def queue_wait_list(df: pd.DataFrame) -> pd.Series:
     input_id = st.session_state['widget_id'].__next__()
 
-    if model.QUEUE_DUR not in df:
-      df[model.QUEUE_DUR] = np.nan
+    q_dur = model.QUEUE_DUR
+
+    if q_dur not in df:
+      df[q_dur] = np.nan
 
     min_wait = st.number_input("Mininum Seconds",
-                               min_value=1,
-                               max_value=99,
+                               min_value=1, max_value=99,
                                value=30,
                                step=1,
                                key=input_id,
@@ -28,7 +29,7 @@ SELECT *, {queue_dur} "Queue Wait"
  FROM df
   WHERE {queue_dur} is not null
     AND {queue_dur} > {queue_wait}
-""".format(queue_dur=model.QUEUE_DUR, queue_wait=min_wait)
+""".format(queue_dur=q_dur, queue_wait=min_wait)
     query = duckdb.query(query).to_df()
     query["Queue Wait"] = query["Queue Wait"].apply(lambda x: time_to_hour_minute_second(seconds=x))
 
